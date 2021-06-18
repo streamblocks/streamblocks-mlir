c87a9fd11d7c25c499a0a112807d0221521af93a
Endri Bezati <endri.bezati@gmail.com>
2021-06-18 11:36:22 +0200

    (Common Dialect) Adding Constant and Add Op on common dialect.

--- include/StreamBlocks/Common/CommonDialect.h
+++ include/StreamBlocks/Common/CommonDialect.h
@@ -9,8 +9,20 @@
 #ifndef COMMON_DIALECT_H
 #define COMMON_DIALECT_H
 
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/CastInterfaces.h"
+#include "mlir/Interfaces/ControlFlowInterfaces.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/VectorInterfaces.h"
+#include "mlir/Interfaces/ViewLikeInterface.h"
 
 #include "StreamBlocks/Common/CommonOpsDialect.h.inc"
+
+using namespace mlir;
 
 #endif // COMMON_DIALECT_H
--- lib/StreamBlocks/Common/CommonDialect.cpp
+++ lib/StreamBlocks/Common/CommonDialect.cpp
@@ -6,6 +6,29 @@
 //
 //===----------------------------------------------------------------------===//
 
+
+
+#include "mlir/Dialect/CommonFolders.h"
+#include "mlir/Dialect/StandardOps/Utils/Utils.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/AffineExpr.h"
+#include "mlir/IR/AffineMap.h"
+#include "mlir/IR/BlockAndValueMapping.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Matchers.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/IR/Value.h"
+#include "mlir/Support/MathExtras.h"
+#include "mlir/Transforms/InliningUtils.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/StringSwitch.h"
+#include "llvm/Support/FormatVariadic.h"
+#include "llvm/Support/raw_ostream.h"
+
 #include "StreamBlocks/Common/CommonDialect.h"
 #include "StreamBlocks/Common/CommonOps.h"
 
